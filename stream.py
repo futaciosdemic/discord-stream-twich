5a5a5046336a8dcc860b139acb67f0332d7d4c50
futmameeh <148143183+futmamencur@users.noreply.github.com>
2024-05-20 18:27:53 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -7,5 +7,6 @@ client = commands.Bot(command_prefix='!', intents=intents) # this is the prefix 
 
 @client.command()
 async def stream(ctx, *, stream_name: str):
-    await client_multi_line_spam.change_presence(status=discord.Status.dnd, activity=discord.Streaming(name=stream_name, url=""))
-    await ctx.send(f"# <@{ctx.author.id}> you re streaming now:  {stream_name}")
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Streaming(name=stream_name, url="https://twitch.tv/your_channel"))
+    await ctx.send(f"<@{ctx.author.id}> you are now streaming: {stream_name}")
+client.run(token)
